4d387a62a82608029e2cb5e8f200fbf00fbb2026
alessandrocharbonnier <haloalessandro230@gmail.com>
2018-11-10 10:28:41 +0100

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+//loading config file
 const { prefix, token } = require('./config.json');
 
+// array of nouns used to generate new channels
 const profs = ['Patoch', 'MHR', 'Pierre', 'Denis',
                'Annabelle', 'Luc', 'Florent', 'Didier',
                'Selma', 'Zina', 'Patricia', 'Oleg', 'Jeremy',
@@ -10,8 +12,10 @@ const profs = ['Patoch', 'MHR', 'Pierre', 'Denis',
                'Regine', 'Jaen-Francois', 'Bibone', 'Frederic',
                'A-preciser', 'Fabrice'];
 
+// channels that will be affected by the bot
 const channels = ['CHAUSSURE', 'e-sport', 'programmation'];
 
+// create a new channel with the noun of the parent category with a random name and place that channel in the appropriate category
 function createNewChannel(newMember, newUserChannel) {
   try {
     let name = newUserChannel.name + '-' + profs[Math.floor(Math.random() * profs.length)];
@@ -23,22 +27,27 @@ function createNewChannel(newMember, newUserChannel) {
   } catch (e) { }
 }
 
+// properlly exit the program is the user hits ctrl+c
 process.on('SIGINT', () => {
     console.log('Good bye!');
     bot.destroy();
     process.exit();
 });
 
+// connect the bot
 bot.login(token);
 
+// log that the bot is ready (optionnal)
 bot.once('ready', () => {
     console.log('Ready!');
 });
 
+// when a user join a channel the bot will add 2 roles (specific for every server)
 bot.on('guildMemberAdd', member => {
     member.addRoles(['499314917711675393', '366669559341645856']);
 });
 
+// when a user join/quit/move channel
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel;
     let oldUserChannel = oldMember.voiceChannel;
